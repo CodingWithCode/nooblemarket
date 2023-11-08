b74ea309301f99e874d3cc082f2c68994b116687
Prime Coder <98280125+CodingWithCode@users.noreply.github.com>
2023-11-08 07:02:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.querySelector(".navbar").style.backgroundColor = "rgb(43, 0, 128)";
+        document.querySelector(".logo-text").style.color = "white";
+        if (window.innerWidth >630) {
+            document.querySelector(".nav-element1").style.color = "white";
+            document.querySelector(".nav-element2").style.color = "white";
+            document.querySelector(".nav-element3").style.color = "white";
+            document.querySelector(".nav-element4").style.color = "white";
+            document.querySelector(".nav-element5").style.color = "white";
+            document.getElementById("close-nav").style.color = "white";
+        }
+        document.getElementById("open-nav").style.color = "white";
+    } else {
+        document.querySelector(".navbar").style.backgroundColor = "rgba(4, 9, 30, 0)";
+        document.querySelector(".logo-text").style.color = "white";
+        if (window.innerWidth > 630) {
+            document.querySelector(".nav-element1").style.color = "white";
+            document.querySelector(".nav-element2").style.color = "white";
+            document.querySelector(".nav-element3").style.color = "white";
+            document.querySelector(".nav-element4").style.color = "white";
+            document.querySelector(".nav-element5").style.color = "white";
+            document.getElementById("close-nav").style.color = "white";
+        }
+        document.getElementById("open-nav").style.color = "white";
+    }
+}
+
+var loader = document.getElementById('loader');
+
+window.addEventListener('load', function(){
+    loader.style.display = 'none';
+});
+
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.getElementById('modal_container');
+
+open.addEventListener('click', () => {
+	modal.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+	modal.classList.remove('show');
+});
+
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+  });
+  
